465bda0becdf19d25fc8670088921aafeb3f32d6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-09-01 07:18:36 +0530

    react commit 7

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increament</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
