34fd405abcf3c55d5a39ef73d3626cf9cb31637e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-09-06 08:19:58 +0530

    counter v9

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,23 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncreament = (counterId) => {
+    const counter = this.state.counters.filter((m) => m.id === counterId); // object that's value will be incremented
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter[0]); // no issues, counter is array, contains only one object as its value
+    counters[index].value = counter[0].value;
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((m) => m.id !== counterId);
     this.setState({ counters });
@@ -19,9 +36,16 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm-m2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
+            onIncreament={this.handleIncreament}
             onDelete={this.handleDelete}
             counter={counter}
           /> // passing value and id from this component to counter component using 'value' and 'id' attribute
